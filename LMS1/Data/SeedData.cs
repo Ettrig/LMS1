cce682b176eed918a3eca5a2f37b284a10cb6abe
CJA <c.arenander@gmail.com>
2019-06-28 16:19:53 +0200

    CJA: First try of seeddata

---
+++ LMS1/Data/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using LMS1.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS1.Data
+{
+    public class SeedData
+    {
+        internal static void Initialize(IServiceProvider services)
+        {
+            var options = services.GetRequiredService<DbContextOptions<ApplicationDbContext>>();
+            using (var context = new ApplicationDbContext(options))
+            {
+                if (context.Course.Any())
+                {
+                    context.Course.RemoveRange(context.Course);
+                    context.CourseModule.RemoveRange(context.CourseModule);
+                }
+
+
+                var courses = new List<Course>();
+                for (int i = 1; i <= 10; i++)
+                {
+                    var courseName = "C" + i + ": " + Faker.Company.CatchPhrase();
+                    var course = new Course
+                    {
+                        Name = courseName,
+                        //Email = Faker.Internet.Email(userName)
+                    };
+                    courses.Add(course);
+                }
+                
+
+                foreach (var course in courses)
+                {
+                    var courseModuleName = "M";
+                    var courseModule = new CourseModule
+                    {
+                        Name = courseModuleName,
+
+                    };
+                }
+
+                context.Course.AddRange(courses);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- LMS1/Program.cs
+++ LMS1/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using LMS1.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace LMS1
@@ -14,7 +16,18 @@ namespace LMS1
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            //CreateWebHostBuilder(args).Build().Run();
+
+            IWebHost webHost = CreateWebHostBuilder(args).Build();
+
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                //Seed
+                SeedData.Initialize(services);
+            }
+
+            webHost.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
